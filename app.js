4f3c8d8fda2bb3dc61c2a45ef344584529759e49
ChloeArd <chloe.ardoise@gmail.com>
2020-12-21 13:31:38 +0100

    Exercice 96

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ function test(a) {
     let div1 = document.createElement("div");
     div1.innerHTML = "La variable a vaut " + a;
     document.body.appendChild(div1);
-    ((a += 2) < 20) ? test(a) : null;
+    ((a += 2) < 30) ? test(a) : null;
 }
 test(1);
